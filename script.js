47dfc397caed9f6b899845e7b0c547fbd3346f6f
Mayank Shukla <117716383+mayankshukla-20@users.noreply.github.com>
2024-01-25 16:40:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+let arr = [1,2,3,4];
+function filter(data){
+	return new Promise((resolve,reject) => {
+		setTimeout(() => {
+			resolve(data.filter((item) => item%2 == 0));
+		},1000)
+	})
+};
+function multiply(data){
+	return new Promise((resolve,reject) => {
+		setTimeout(() => {
+			resolve(data.map((item) => item*2);
+		},2000)
+	})
+}
+
+async function xyz(){
+	let data1 = await filter(arr);
+	document.getElementById("output").innerHTML += `<p>${data1}</p>`;
+	let data2 = await multiply(data1);
+	document.getElementById("output").innerHTML += `<p>${data2}</p>`;
+}
+xyz();
